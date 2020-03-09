3ef96809f052f8f1e903f07ff90a0e987010da61
Adela Darmansyah <adela.yohana@gmail.com>
2020-03-09 10:05:02 -0700

    Create a form, routes and models
    Co-authored-by: T Siu <tsiu88@gmail.com>

--- Madlibs.Solution/Madlibs/Controllers/HomeController.cs
+++ Madlibs.Solution/Madlibs/Controllers/HomeController.cs
@@ -5,7 +5,20 @@ namespace Madlibs.Controllers
 {
   public class HomeController : Controller
   {
+    [Route("/")]
+    public ActionResult Index(string noun1, string noun2, string noun3, string nounPlural, string place, string adjective)
+    {
+      Story myStory = new Story();
+      myStory.Noun1 = noun1;
+      myStory.Noun2 = noun2;
+      myStory.Noun3 = noun3;
+      myStory.NounPlural = nounPlural;
+      myStory.Place = place;
+      myStory.Adjective = adjective;
+      return View(myStory);
+    }
 
-
+    [Route("/form")]
+    public ActionResult Form() { return View(); }
   }
 }
--- Madlibs.Solution/Madlibs/Models/Story.cs
+++ Madlibs.Solution/Madlibs/Models/Story.cs
@@ -5,6 +5,11 @@ namespace Madlibs.Models
 {
   public class Story
   {
-
+    public string Noun1 { get; set; }
+    public string NounPlural { get; set; }
+    public string Noun2 { get; set; }
+    public string Place { get; set; }
+    public string Adjective { get; set; }
+    public string Noun3 { get; set; }
   }
 }
